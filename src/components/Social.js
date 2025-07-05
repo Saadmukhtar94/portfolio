57177758be5237d752d8e058bba684f6d1b50ba4
Saadmukhtar94 <saadmukhtar584@gmail.com>
2025-07-06 01:27:22 +0500

    linkedin link corrected

--- src/components/Social.js
+++ src/components/Social.js
@@ -6,7 +6,7 @@ export default function Page () {
       <div className="cardsocial bg-primary" id="socialIcons-container">
         <span><i className="fa-solid fa-link fs-6 mx-1"></i>Social</span>
         <div className="d-flex gap-2 ">
-         <Link href="www.linkedin.com/in/saad-mukhtar-a218b6363" className="list-unstyled social-link" target='_blank'><i className="fa-brands fa-linkedin fs-3"></i></Link>
+         <Link href="https://www.linkedin.com/in/saad-mukhtar-a218b6363" className="list-unstyled social-link" target='_blank'><i className="fa-brands fa-linkedin fs-3"></i></Link>
           <Link href="https://www.facebook.com/saad.m.7771/" className="list-unstyled social-link" target='_blank'><i className="fa-brands fa-square-facebook fs-3 "></i></Link>
          
          {/* <Link href="/" className="list-unstyled social-link"><i className="fa-brands fa-square-instagram fs-3 "></i></Link> */}
